65dc9a0104b2f83a9ec4776b62585b69e282b0a6
villacresbryan <villacresbryan1a@gmail.com>
2021-12-10 19:13:19 -0500

    Final.sh

--- netsize.sh
+++ netsize.sh
@@ -3,6 +3,9 @@ echo " Escriba un guión que tome uno de estos archivos y determine el número d
 (polinizadores) y columnas (plantas). Tenga en cuenta que las columnas están
 separadas por espacios y que hay un espacio al final de cada línea"
 cd ../../
+cd Documents/Bioinformatica/CSB/unix/data/Saavedra2013
+echo
+echo
 echo "numero de filas"
 echo
 
@@ -10,5 +13,5 @@ echo
 
 echo "numero de columnas"
 
-     head -n1 ../Saavedra2013/n1.txt | tr -d " " | tr -d "\ n" | wc -c
+     head -n1 ../Saavedra2013/n1.txt | tr -d " " | tr -d "\n" | wc -c
 
