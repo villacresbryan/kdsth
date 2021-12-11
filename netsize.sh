1adc416066ad3bf79a76c708e3338e4cbbce63a8
villacresbryan <villacresbryan1a@gmail.com>
2021-12-10 22:08:38 -0500

    final 5

--- netsize.sh
+++ netsize.sh
@@ -4,14 +4,17 @@ echo " Escriba un guión que tome uno de estos archivos y determine el número d
 separadas por espacios y que hay un espacio al final de cada línea"
 cd ../../
 cd Documents/Bioinformatica/CSB/unix/data/Saavedra2013
+#se encuentra la carpeta Saavedra2013
 echo
 echo
 echo "numero de filas"
 echo
 
      cat ../Saavedra2013/n1.txt | wc -l
-
+# comando cat para contar, comando wc - L para contar las lineas
 echo "numero de columnas"
 
      head -n1 ../Saavedra2013/n1.txt | tr -d " " | tr -d "\n" | wc -c
+
+#head -n1 se usa para seleccionar la primera fila,  se uso tr -d " " para eliminar los espacios y "\n"  para eliminar los espacios finales,  el comando wc-c para contar los caracteres.
 
